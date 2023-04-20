2151fcc1224ed0da341d59c1536389afc13cb8e0
sainathmcs <sainath.mcs@gmail.com>
2023-04-20 17:06:04 -0400

    port

--- main.py
+++ main.py
@@ -355,7 +355,8 @@ def upload():
 
 
 def run_flask():
-    app.run(debug=False)
+    # run flask app with debug mode and port 8000 with host 0.0.0.0
+    app.run(debug=True, port=8000, host='0.0.0.0')
 
 
 if __name__ == '__main__':
